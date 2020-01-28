b908cd2710ab1fdd65d90b05bee9ef691f06b3e8
NickReva <59935905+NickAReva@users.noreply.github.com>
2020-01-28 21:54:24 +0400

    Add some JavaScript
    Modify  component and observe what happens in the console

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+    const App = () =>{
+      console.log('Hello from component')
+      return(
+        <div>
+          <p>Hello World</p>
+        </div>
+      )
+    }
 
-const App = () =>(
-  <div>
-    <p>Hello World</p>
-  </div>
-)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
