985ec30d97d7deed731a66e22ffa431edf56211b
NickReva <59935905+NickAReva@users.noreply.github.com>
2020-02-01 21:01:55 +0400

    Modify the component
    I added some code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
     const App = () =>{
-      console.log('Hello from component')
+      const now = new Date()
+      const a = 10
+      const b = 20
+
       return(
         <div>
           <p>Hello World</p>
+          <p>
+          {a} plus {b} is {a + b}
+          </p>
         </div>
       )
     }
