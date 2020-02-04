3d0e5b2aba464e8406918d89698cc0254d99b70e
NickReva <59935905+NickAReva@users.noreply.github.com>
2020-02-04 22:30:36 +0400

    Adding Footer const
    Added new const Footer

--- src/index.js
+++ src/index.js
@@ -9,6 +9,14 @@ import ReactDOM from 'react-dom';
       </div>
     )
   }
+  const Footer = () =>{
+    return (
+      <div>
+        greeting app created by
+        <a href="https://github.com/mluukkai">mluukkai</a>
+      </div>
+    )
+  }
   const App = () =>{
     const name='Peter'
     const age= 10
@@ -18,7 +26,7 @@ import ReactDOM from 'react-dom';
           <h1>Greatings</h1>
           <Hello name="Maya" age={26+10} feature="pretty"/>
           <Hello name={name} age={age} feature={feature}/>
-          <Hello />
+          <Footer />
         </div>
       )
     }
